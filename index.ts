461f2b948ef8b83f58793452c7603d1958492141
Brenorml <breno.lucena@gmail.com>
2022-07-08 14:37:27 +0100

    Analisando type para juntar interfaces com polimorfismo

--- index.ts
+++ index.ts
@@ -1,33 +1,23 @@
-/* function soma (a: number, b: number) { //variáveis podem ser tipadas
-    return a + b;
-}
-
-soma('a', 'b'); */
-
-//types 
-//interfaces
-
 interface IAnimal {
     nome: string;
     tipo: 'terrestre' | 'aquático';
-    executarRugido(alturaEmDecibeis: number): void;
-}
+    domestico: boolean;
+} 
 
 interface IFelino extends IAnimal {
     visaoNoturna: boolean;
 }
 
-//Atalho CTRL + es+aço, indica quais as opções contidas na interface Ianimal. Assim como as opções contidas nas promisses.
-const animal: IAnimal = { 
-    nome: 'Elefante',
-    tipo: "terrestre",
-    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+interface ICanino extends IAnimal {
+    porte: 'pequeno' | 'medio' | 'grande';
 }
 
-animal.executarRugido('s');
+type IDomestico = IFelino | ICanino;
 
-const felino: IFelino = {
-    nome: 'Leão',
-    tipo: "terrestre",
-    visaoNoturna: true;
+const animal: IDomestico = { 
+    domestico: true,
+    nome: 'cachorro',
+    porte: 'medio',
+    tipo: 'terrestre',
+    visaoNoturna: true,
 }
