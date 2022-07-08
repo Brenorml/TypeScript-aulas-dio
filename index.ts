2f094b1aeca8eedc446393e47058259a01a67744
Brenorml <breno.lucena@gmail.com>
2022-07-08 13:39:24 +0100

    criando interfaces, analisando o polimorfismo e verificando novos erros

--- index.ts
+++ index.ts
@@ -1,5 +1,33 @@
-function soma (a: number, b: number) {
+/* function soma (a: number, b: number) { //variáveis podem ser tipadas
     return a + b;
 }
 
-soma('a', 'b');
+soma('a', 'b'); */
+
+//types 
+//interfaces
+
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'aquático';
+    executarRugido(alturaEmDecibeis: number): void;
+}
+
+interface IFelino extends IAnimal {
+    visaoNoturna: boolean;
+}
+
+//Atalho CTRL + es+aço, indica quais as opções contidas na interface Ianimal. Assim como as opções contidas nas promisses.
+const animal: IAnimal = { 
+    nome: 'Elefante',
+    tipo: "terrestre",
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+}
+
+animal.executarRugido('s');
+
+const felino: IFelino = {
+    nome: 'Leão',
+    tipo: "terrestre",
+    visaoNoturna: true;
+}
